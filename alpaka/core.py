551589e239c690c03fe0c9e5a4b3007e428c8157
Daniel Firer <danielfirefire@gmail.com>
2024-10-04 23:47:27 +0300

    improve mapping via propagation

--- alpaka/core.py
+++ alpaka/core.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import logging
 from typing import Dict, List
 
@@ -12,6 +13,31 @@ from .heckel_diff import diff as heckel_diff
 
 logger = logging.getLogger(__name__)
 
+def _get_vote_for_types(type_a, type_b):
+    type_a = type_a.underlying_array_type
+    type_b = type_b.underlying_array_type
+
+    if type_a.type != DEX.Type.TYPES.CLASS or type_b.type != DEX.Type.TYPES.CLASS:
+        return None
+
+    return str(type_a), str(type_b)
+
+def _gather_votes(class_a, class_b):
+    for field_a, field_b in zip(class_a.fields, class_b.fields):
+        vote = _get_vote_for_types(field_a.type, field_b.type)
+        if vote is not None:
+            yield vote
+    
+    for method_a, method_b in zip(class_a.methods, class_b.methods):
+        vote = _get_vote_for_types(method_a.prototype.return_type, method_b.prototype.return_type)
+        if vote is not None:
+            yield vote
+        
+        for param_a, param_b in zip(method_a.prototype.parameters_type, method_b.prototype.parameters_type):
+            vote = _get_vote_for_types(param_a, param_b)
+            if vote is not None:
+                yield vote
+
 def map(only_obfuscated: bool, classes_a, classes_b):
     logger.info(f'classes in input A: {len(classes_a)}')
     logger.info(f'classes in input B: {len(classes_b)}')
@@ -21,9 +47,27 @@ def map(only_obfuscated: bool, classes_a, classes_b):
 
     mapping, reverse_mapping = heckel_diff(encodings_a, encodings_b)
 
+    logger.info(f'heckel diff mapped classes: {len(mapping)}')
+
+    classes_mapping = {classes_a[k]: classes_b[v] for k, v in mapping.items()}
+    votes = Counter(vote for class_a, class_b in classes_mapping.items() for vote in _gather_votes(class_a, class_b))
+
+    logger.info(f'propagation votes: {len(votes)} ({votes.total()} total)')
+
     mapping = {classes_a[k].fullname: classes_b[v].fullname for k, v in mapping.items()}
 
-    logger.info(f'heckel diff mapped classes: {len(mapping)}')
+    mapped_a = set(mapping.keys())
+    mapped_b = set(mapping.values())
+
+    for class_a, class_b in sorted(votes, key=votes.get):
+        if class_a in mapped_a or class_b in mapped_b:
+            continue
+
+        mapping[class_a] = class_b
+        mapped_a.add(class_a)
+        mapped_b.add(class_b)
+
+    logger.info(f'propagation mapped classes: {len(mapping)}')
 
     if only_obfuscated:
         mapping = {k: v for k, v in mapping.items() if is_obfuscated_class_name(k)}
